b62c1b70734fe2045f9b29903e334a5117f42293
Luis Enrique Solis Puc <sop199642@gmail.com>
2021-05-02 00:19:46 -0500

    Se realizo la instalacion y configuracion del Auth y la instalacion y configuracion de roles y permisos

---
+++ database/seeds/RoleSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $role1 = Role::create(['name' => 'admin']);
+        $role2 = Role::create(['name' => 'usuario']);
+        
+        $permission1 = Permission::create(['name' => 'home.index']);
+        $permission1->syncRoles([$role1, $role2]);
+    }
+}
